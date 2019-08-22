f8397fbff11538f8a25e7986d7278f4f1beb5f86
Iseli Stefan <stefan.iseli@bfh.ch>
2019-08-22 10:08:38 +0200

    initial commit for web wrapper

---
+++ web/src/hello.ts
@@ -0,0 +1,9 @@
+export class Hello {
+    constructor() {
+
+    }
+
+    sayIt() {
+        return "Gugguseli";
+    }
+}
---
+++ web/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require("path");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var PATHS = {
+    entryPoint: path.resolve(__dirname, 'src/index.ts'),
+    bundles: path.resolve(__dirname, '_bundles'),
+}
+
+var config = {
+    entry: {
+        'web-on-fhir': [PATHS.entryPoint],
+    },
+    // When including the bundle in the browser it will be accessible at `window.web-fhir`
+    output: {
+        path: PATHS.bundles,
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'web-on-fhir',
+        umdNamedDefine: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimizer: [
+            // we specify a custom UglifyJsPlugin here to get source maps in production
+            new UglifyJsPlugin({
+              cache: true,
+              parallel: true,
+              uglifyOptions: {
+                compress: false,
+                ecma: 6,
+                mangle: true
+              },
+              sourceMap: true
+            })
+          ]
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+            exclude: /node_modules/,
+            query: {
+                declaration: false,
+            }
+        }]
+    }
+}
+
+module.exports = config;
